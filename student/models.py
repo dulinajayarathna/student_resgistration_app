aa90027bb6ecdf4e34e3f8baa8d2a9e49549ffff
Dulina jayarathna <jayarathnadulina@gmail.com>
2024-08-01 22:57:19 +0530

    feat(PDO-106):adding comments

--- student/models.py
+++ student/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Student(models.Model):
+    """
+    A model representing a student with the following attributes:
+    - first_name: The student's first name (max length: 20 characters)
+    - last_name: The student's last name (max length: 20 characters)
+    - dob: The student's date of birth
+    - email: The student's unique email address
+    """
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     dob = models.DateField()
     email = models.EmailField(unique=True)
-
