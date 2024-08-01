6dfd5b1a814ef1ce078020af56fc8832ebae6eac
Dulina jayarathna <jayarathnadulina@gmail.com>
2024-08-01 22:43:18 +0530

    feat(PDO-105):load environment variables are at of the settings.py

--- student/models.py
+++ student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    dob = models.DateField()
+    email = models.EmailField(unique=True)
+
