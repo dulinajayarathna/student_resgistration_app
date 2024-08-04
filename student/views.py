48b65e212cfa4a9fe82172ffa2aac6512fe54992
Dulina jayarathna <jayarathnadulina@gmail.com>
2024-08-04 17:02:41 +0530

    feat(PDO-108):updated the readme file

--- student/views.py
+++ student/views.py
@@ -4,6 +4,6 @@ from .models import Student
 from .serializers import StudentSerializer
 
 
-class StudentViewSet(viewsets.ModelViewSet):
+class StudentViewSet(viewsets.ModelViewSet):  # Define the queryset to retrieve all Student objects from the database
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
